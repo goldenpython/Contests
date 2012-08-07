e7650eb16e80eb24100581ac2ef7229a92cbf7de
GoldenPython <cralexandrescu@gmail.com>
2012-08-07 23:33:31 +0300

    Problem 3 & 4

---
+++ PrjEuler/003/003.py
@@ -0,0 +1,21 @@
+################################################################################
+#	Cristian Alexandrescu
+#	2163013577ba2bc237f22b3f4d006856
+#	11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
+#	bc9a53289baf23d369484f5343ed5d6c
+################################################################################
+
+def Solve():
+	Number = 600851475143;
+	Factor = 2; LastPrime = 0;
+	while (Factor <= Number):
+		while (Number % Factor == 0):
+			Number /= Factor;
+			LastPrime = Factor;
+		Factor = Factor + 1;
+		
+	print (LastPrime);
+
+	
+print ("PROJECT EULER 003:");
+Solve();
---
+++ PrjEuler/004/004.py
@@ -0,0 +1,37 @@
+################################################################################
+#	Cristian Alexandrescu
+#	2163013577ba2bc237f22b3f4d006856
+#	11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
+#	bc9a53289baf23d369484f5343ed5d6c
+################################################################################
+
+def Solve():
+	Largest	= 0;
+	
+	for i in range(999, 100, -1):
+		if (i * 999 < Largest):
+			break;
+			
+		for j in range(999, 100, -1):
+			n = i * j;
+			if (IsPalindrome(n) and Largest < n):
+				Largest = n;
+				
+	print(Largest);
+	
+def IsPalindrome(Number):
+	s = str(Number);
+	i, j = 0, len(s) - 1;
+	
+	while (i < j):
+		if (s[i] != s[j]):
+			return False;
+			
+		i = i + 1;
+		j = j - 1;
+		
+	return True;
+
+
+print ("PROJECT EULER 004:");
+Solve();
