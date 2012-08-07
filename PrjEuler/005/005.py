51554932ba195ceb82e42037125f135fffd314b2
GoldenPython <cralexandrescu@gmail.com>
2012-08-07 23:36:50 +0300

    Problem 5

---
+++ PrjEuler/005/005.py
@@ -0,0 +1,20 @@
+################################################################################
+#	Cristian Alexandrescu
+#	2163013577ba2bc237f22b3f4d006856
+#	11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
+#	bc9a53289baf23d369484f5343ed5d6c
+################################################################################
+
+import fractions
+
+def Solve():
+	n = 1;
+	for i in range(1, 20):
+		n = n * i / fractions.gcd(n, i);
+	
+	print(int(n));
+
+
+	
+print ("PROJECT EULER 005:");
+Solve();
