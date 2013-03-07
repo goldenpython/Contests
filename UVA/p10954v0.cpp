731d87659d7e7c6727bd6e07aecbb6f2494a182e
GoldenPython <cralexandrescu@gmail.com>
2013-03-07 22:17:46 +0200

    UVA 10954
    Problem 10954 - Add All
    C++ language version
    
    2163013577ba2bc237f22b3f4d006856
    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
    bc9a53289baf23d369484f5343ed5d6c

---
+++ UVA/p10954v0.cpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+*    Cristian Alexandrescu                                                     *
+*    2163013577ba2bc237f22b3f4d006856                                          *
+*    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385          *
+*    bc9a53289baf23d369484f5343ed5d6c                                          *
+*******************************************************************************/
+
+/*   Problem 10954 - Add All                                                  */
+
+
+
+
+#include <stdio.h>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int main() {
+	int nN;
+	priority_queue<long long, vector<long long>, greater<long long> > oPQ;
+
+	while (scanf("%d", &nN), nN) {
+
+		for (int nLoop = 0; nLoop < nN; nLoop++) {
+			int nNumber;
+			scanf("%d", &nNumber);
+			oPQ.push(nNumber);
+		}
+
+		long long llSol = 0;
+		while (oPQ.size() > 1) {
+			long long llNumber = oPQ.top();
+			oPQ.pop();
+
+			llNumber += oPQ.top();
+			oPQ.pop();
+
+			llSol += llNumber;
+			oPQ.push(llNumber);
+		}
+
+		printf("%lld\n", llSol);
+		oPQ.pop();
+	}
+
+	return 0;
+}
