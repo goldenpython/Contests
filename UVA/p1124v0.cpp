a495e9fd4abf953d7c86353e747c72d6247bc8aa
GoldenPython <calexandrescu@gmail.com>
2013-08-25 18:52:20 +0300

    UVA 1124
    Problem 1124 - Celebrity jeopardy
    C++ language version
    
    2163013577ba2bc237f22b3f4d006856
    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
    bc9a53289baf23d369484f5343ed5d6c

---
+++ UVA/p1124v0.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+*    Cristian Alexandrescu                                                     *
+*    2163013577ba2bc237f22b3f4d006856                                          *
+*    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385          *
+*    bc9a53289baf23d369484f5343ed5d6c                                          *
+*******************************************************************************/
+
+/*   Problem 1124 - Celebrity jeopardy                                        */
+
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string line;
+
+	while (getline(cin, line))
+		cout << line << endl;
+
+	return 0;
+}
