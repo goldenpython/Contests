bbc7b36a6e92b9397186df82037ed223d4fb6b6c
GoldenPython <calexandrescu@gmail.com>
2013-10-12 02:34:57 +0300

    UVA 11683
    Problem 11683 - Laser Sculpture
    C++ language version
    
    2163013577ba2bc237f22b3f4d006856
    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
    bc9a53289baf23d369484f5343ed5d6c

---
+++ UVA/p11683v0.cpp
@@ -0,0 +1,37 @@
+/*******************************************************************************
+*    Cristian Alexandrescu                                                     *
+*    2163013577ba2bc237f22b3f4d006856                                          *
+*    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385          *
+*    bc9a53289baf23d369484f5343ed5d6c                                          *
+*******************************************************************************/
+
+/*   Problem 11683 - Laser Sculpture                                          */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int nA, nC;
+
+	while (true) {
+		cin >> nA;
+		if (!nA)
+			break;
+		cin >> nC;
+
+		int nLast = nA, nSol = 0;
+		for (int nLoop = 0; nLoop < nC; nLoop++) {
+			int nCurrent;
+			cin >> nCurrent;
+			if (nCurrent < nLast)
+				nSol += nLast - nCurrent;
+			nLast = nCurrent;
+		}
+
+		cout << nSol << endl;
+	}
+
+	return 0;
+}
