f209ce7d86f85ef502d05173f14bab53ac6d484e
GoldenPython <calexandrescu@gmail.com>
2016-03-23 00:39:51 +0200

    UVA 13059
    Problem 13059 - Tennis Championship
    C++ language version
    
    2163013577ba2bc237f22b3f4d006856
    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
    bc9a53289baf23d369484f5343ed5d6c

---
+++ UVA/p13059v0.cpp
@@ -0,0 +1,18 @@
+/*******************************************************************************
+*    Cristian Alexandrescu                                                     *
+*    2163013577ba2bc237f22b3f4d006856                                          *
+*    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385          *
+*    bc9a53289baf23d369484f5343ed5d6c                                          *
+*******************************************************************************/
+
+/*   Problem 13059 - Tennis Championship                                      */
+
+
+#include <iostream>
+
+int main() {
+	for (long long llN; std::cin >> llN; std::cout << llN - 1 << std::endl)
+		;
+
+	return 0;
+}
