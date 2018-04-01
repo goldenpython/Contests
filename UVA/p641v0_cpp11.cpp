c1ca3dde29d1dd6b068e63de066d8e6a3334f1a7
C. Alexandrescu <calexandrescu@gmail.com>
2018-04-01 23:19:37 +0300

    UVA 641
    Problem 641 - Do the Untwist
    C++11 language version
    
    2163013577ba2bc237f22b3f4d006856
    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385
    bc9a53289baf23d369484f5343ed5d6c

---
+++ UVA/p641v0_cpp11.cpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+*    Cristian Alexandrescu                                                     *
+*    2163013577ba2bc237f22b3f4d006856                                          *
+*    11a4bb2c77aca6a9927b85f259d9af10db791ce5cf884bb31e7f7a889d4fb385          *
+*    bc9a53289baf23d369484f5343ed5d6c                                          *
+*******************************************************************************/
+
+/*   Problem 641 - Do the Untwist                                             */
+
+
+/*
+	ciphercode[i] = (plaincode[ki mod n] - i) mod 28	==>
+	(ciphercode[i] + i) mod 28 = (plaincode[ki mod n]) mod 28
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+	if (c == '_')
+		return 0;
+	else if (c == '.')
+		return 27;
+	else
+		return c - 'a' + 1;
+}
+
+	if (nN == 0)
+		return '_';
+	else if (nN == 27)
+		return '.';
+	else
+		return 'a' + nN - 1;
+}
+
+int main() {
+	for (int nK; (cin >> nK), nK; ) {
+		string oCiphertext;
+		cin >> oCiphertext;
+
+		for (int nIndex = 0, nLen = static_cast<int>(oCiphertext.size()); nIndex < nLen; nIndex++)
+			oPlainText[(nK * nIndex) % nLen] = IntCodeToChar((CharToIntCode(oCiphertext[nIndex]) + nIndex) % 28);
+
+		cout << oPlainText << endl;
+	}
+	return 0;
+}
